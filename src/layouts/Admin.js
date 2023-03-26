a98bd17de29e585e75fd80364714eaa02f8253c6
Gordon Nguyen <maixanh45@yahoo.com>
2023-03-26 00:13:44 -0500

    Modified layout

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -6,6 +6,7 @@ import Footer from "../components/Footer/Footer.js";
 import Sidebar from "../components/Sidebar/Sidebar.js";
 import routes from "../routes.js";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/animate.min.css";
 import "../assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "../assets/css/demo.css";
