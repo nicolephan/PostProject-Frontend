aca789b6048d6e09dd9213722e8d0438a24b17b4
fygure <92011471+fygure@users.noreply.github.com>
2023-03-28 03:32:26 -0500

    added proxy server to handle CORS, tested get request on homepage with button, usestates and hooks

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,57 +11,73 @@
 // }
 
 import React, { useRef, useState } from 'react';
+import axios from 'axios';
 import './Home.css';
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState(null);
   const inputRef = useRef();
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    setInputValue(inputRef.current.value.trim());
+  const handleFormSubmit = async (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    const options = {
+        method: 'GET',
+        url: '/api/users', // Use the /api prefix
+        headers: {'Content-Type': 'application/json'}
+      };
+      
+      try {
+        const response = await axios.request(options);
+        setResult(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
   };
 
   return (
     <div className="hTrackPackage">
       <h1>Track A Package</h1>
 
-      
       <form onSubmit={handleFormSubmit}>
         <div className='input-group'>
-            <input type="text" ref={inputRef} />
-            <button type="submit">Submit</button>
+          <input type="text" ref={inputRef} />
+          <button type="submit">Submit</button>
         </div>
       </form>
-      
-
 
       <div>
-      {inputValue && <p>You entered: {inputValue}</p>}
+        {result && (
+          <p>
+            API response:
+            <pre>{JSON.stringify(result, null, 2)}</pre>
+          </p>
+        )}
       </div>
     </div>
   );
 }
 
-function SearchBar(props){
-    const query = useRef();
+// function SearchBar(props){
+//     const query = useRef();
 
-    const handleSearch =(e) => {
-        e.preventDefault();
-        const queryVal = query.current.value;
-        props.fetchID(queryVal.trim());
-    };
+//     const handleSearch =(e) => {
+//         e.preventDefault();
+//         const queryVal = query.current.value;
+//         props.fetchID(queryVal.trim());
+//     };
 
-    return(
-        <form action="/" method="get">
-            <label htmlFor="header-search"></label>
-            <input
-                type="text"
-                id="header-search"
-                placeholder="Package #ID"
-                name="s"
-            />
-            <button type="submit" onSubmit={handleSearch}>Enter</button>
-        </form>
-    );
-}
+//     return(
+//         <form action="/" method="get">
+//             <label htmlFor="header-search"></label>
+//             <input
+//                 type="text"
+//                 id="header-search"
+//                 placeholder="Package #ID"
+//                 name="s"
+//             />
+//             <button type="submit" onSubmit={handleSearch}>Enter</button>
+//         </form>
+//     );
+// }
---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use('/api', createProxyMiddleware({ 
+      target: 'http://localhost:5000/', 
+      changeOrigin: true,
+      onProxyRes: function (proxyRes, req, res) {
+        res.header('Access-Control-Allow-Origin', '*');
+      }
+    }));
+  };
