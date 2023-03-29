fdca1681c706761796a8cd804aa1cfacb23666a5
fygure <92011471+fygure@users.noreply.github.com>
2023-03-29 05:22:54 -0500

    no post requests

--- src/components/protectedRoutes.js
+++ src/components/protectedRoutes.js
@@ -11,27 +11,27 @@ const ProtectedRoutes = () => {
   //console.log(role);
 
   //TODO FIX PREFLIGHT REQUEST ON BACKEND SIDE
-  const fetchData = async () => {
+  // const fetchData = async () => {
     
-    const token = localStorage.getItem('access_token');
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    const options = {
-      method: 'GET',
-      url: 'https://postoffice-api.herokuapp.com/api/admin', // Use the /api prefix
-      headers: {'Content-Type': 'application/json', Authorization: `Bearer ${token}`,}
-    };
+  //   const token = localStorage.getItem('access_token');
+  //   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://postoffice-api.herokuapp.com/api/admin', // Use the /api prefix
+  //     headers: {'Content-Type': 'application/json', Authorization: `Bearer ${token}`,}
+  //   };
 
-    try {
-      const response = await axios.request(options);
-      console.log(response.data); // print the response data
-    } catch (error) {
-      console.log(error); // handle the error
-    }
-  };
+  //   try {
+  //     const response = await axios.request(options);
+  //     console.log(response.data); // print the response data
+  //   } catch (error) {
+  //     console.log(error); // handle the error
+  //   }
+  // };
   
   
-  console.log('here');
-  fetchData();
+  // console.log('here');
+  // fetchData();
 
   if (role === 'admin')
   {
