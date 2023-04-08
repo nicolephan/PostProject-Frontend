d039a81e82d1e57295c8c914ad1b5433a08e2134
Gordon Nguyen <maixanh45@yahoo.com>
2023-04-08 17:52:27 -0500

    Shipment renders

--- src/pages/Employee/AllShipment.js
+++ src/pages/Employee/AllShipment.js
@@ -1,24 +1,49 @@
 import React, { useState, useEffect } from "react";
 
+// Return All shipments from database
 const AllShipment = () => {
-  const [shipmentData, setShipmentData] = useState({});
-  // Return All shipments
+  const [shipmentData, setShipmentData] = useState([]);
+  
 
   useEffect(() => {
     const fetchShipment = async () => {
-      const res = await fetch("https://postoffice-api.herokuapp.com/api/");
+      const res = await fetch("https://postoffice-api.herokuapp.com/api/shipments");
       const data = await res.json();
       setShipmentData(data);
       console.log(data);
     };
-  });
+    fetchShipment();
+  }, []);
 
   return (
     <>
-      <h1>allshipment</h1>
-      {shipmentData.map((shipmentData) => {
-        return;
-      })}
+      <h1>All Shipment</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Tracking ID</th>
+            <th>Creation date</th>
+            <th>Location</th>
+            <th>Shipment status</th>
+            <th>Number of packages</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*Iterate through shipment json data 
+          and render to front end*/}
+          {shipmentData.map((shipment) => {
+            return (
+              <tr>
+                <th>{shipment.tracking_id}</th>
+                <th>{shipment.creation_date}</th>
+                <th>{shipment.current_location}</th>
+                <th>{shipment.shipment_status}</th>
+                <th>{shipment.num_packages}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
