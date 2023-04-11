19d3af11849be0602d45ae616d85297cc3395173
fygure <92011471+fygure@users.noreply.github.com>
2023-04-11 17:03:53 -0500

    shipping form modal added to admin page

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -1,13 +1,14 @@
 import AdminNav from "../../components/AdminNav/AdminNav";
-
+import ShippingModal from "../../components/Modal/ShippingModal";
 
 export default function Admin(){
+    var x = localStorage.getItem("email");
+
     return (
         <>
-        <AdminNav />
-        <div>
-            <h2>ADMIN</h2>
-            <p>
+            <AdminNav />
+            <h1>ADMIN ({x})</h1>
+            {/* <p>
                 Contains <br />
                 <li>employee information  </li>
                 <li>info about current shipments out</li>
@@ -15,8 +16,10 @@ export default function Admin(){
                 <li>info about all PO boxes (avaliable and otherwise)</li>
                 <li>form to update shipping information</li>
                 <br/>all of which takes info from the db
-            </p>
-        </div>
+            </p> */}
+            <ShippingModal />
+
+
         </>
     );
 }
