7ee7dffb07b1add1a5f14ecb9233d365ae03bfcd
Gordon Nguyen <maixanh45@yahoo.com>
2023-04-14 21:19:14 -0500

    Added email title to modal

--- src/components/Modals/ReadShipmentModal.jsx
+++ src/components/Modals/ReadShipmentModal.jsx
@@ -114,12 +114,12 @@ const ReadShipmentModal = () => {
 
             <button type="submit" disabled={!validateForm()}>Submit</button>
         </form>
-        
+
         {/*Conditional Rendering*/}
         {
           emailresult && (
         <>
-          <h1>Email Shipment</h1>
+          <h4>All shipments from <br/>Email: {email}</h4>
           <table>
             <thead>
               <tr>
@@ -137,7 +137,7 @@ const ReadShipmentModal = () => {
                 return (
                   <tr>
                     <th>{shipment.shipment_tracking_id}</th>
-                    <th>{shipment.tracking_status}</th>
+                    <th>{String(shipment.tracking_status)}</th>
                     <th>{shipment.est_delivery_date.slice(0,10)}</th>
                     <th>{shipment.shipment_status}</th>
                     <th>{shipment.num_packages}</th>
@@ -153,13 +153,13 @@ const ReadShipmentModal = () => {
         {
           result && (
         <>
-          <h1>All Shipment</h1>
+          <h4>All Shipment</h4>
           <table>
             <thead>
               <tr>
                 <th>Tracking ID</th>
                 <th>Creation date</th>
-                <th>Location</th>
+                <th>Current location</th>
                 <th>Shipment status</th>
                 <th>Number of packages</th>
               </tr>
